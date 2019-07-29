c0073c58eb91cbc2a7fa0314cff37a659a702d2e
Kirk Vasilas <35264676+kvasilas@users.noreply.github.com>
2019-07-29 13:01:19 -0400

    Create emails.js

---
+++ nodejs/emails.js
@@ -0,0 +1,16 @@
+var message = { 
+    // sender info
+    from: '121llkkjsdf <121llkkjsdf@mytestsite.com>',
+
+    // Comma separated list of recipients
+    to: '<someone@somewhere.com>',
+
+    // Subject of the message
+    subject: '', //
+
+    // plaintext body
+    text: '',
+
+    // HTML body
+    html: ''
+    };
